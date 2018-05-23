7e24464aac8861928886c2ad519177a461d8a81f
markosopcic <marko.sopcic@fer.hr>
2018-05-23 17:43:14 +0200

    ne pitaj

--- app/src/main/java/hr/fer/drinkinggame/higherlower/CardDeck.java
+++ app/src/main/java/hr/fer/drinkinggame/higherlower/CardDeck.java
@@ -15,44 +15,52 @@ import java.util.List;
 
 public class CardDeck {
     public static List<String> strengths= Arrays.asList("2","3","4","5","6","7","8","9","10","jack","queen","king","ace");
-   private List<Card> cards;
-   private List<Card> usedCards;
+   private List<String> cards;
+   private List<String> usedCards;
+   private AssetManager manager;
+   private DisplayMetrics dm;
 
     public CardDeck(AssetManager manager, DisplayMetrics dm){
         String[] c=null;
+        this.manager=manager;
+        this.dm=dm;
         usedCards=new ArrayList<>();
         try {
              c = manager.list("cards");
         }catch(Exception e){
 
         }
-        cards=new ArrayList<Card>();
-        int cardHeight,cardWidth;
-        cardHeight=(int)(dm.heightPixels*0.4);
-        cardWidth=(int)(dm.widthPixels*0.4);
-        for(String s:c){
-            Bitmap d=null;
-            try {
-                 d = BitmapFactory.decodeStream(manager.open("cards/"+s));
-                 d=Bitmap.createScaledBitmap(d,cardWidth,cardHeight,false);
-            }catch(Exception e){}
-          String[] spl=s.split("/");
+        cards=new ArrayList<String>();
 
-          Card card=new Card(d,spl[spl.length-1].split("\\.")[0],null);
-          cards.add(card);
+
+        for(String s:c){
+           cards.add(s);
         }
     }
 
     public Card pullCardAtIndex(int index){
         if(index>=0 && index<cards.size()){
-            Card c=cards.get(index);
-            usedCards.add(c);
-            cards.remove(c);
-            if(cards.size()==0){
-                cards=usedCards;
-                usedCards.clear();
-            }
-            return c;
+            Bitmap d=null;
+            try {
+                String s=cards.get(index);
+                cards.remove(s);
+                usedCards.add(s);
+                if(cards.isEmpty()){
+                    cards=usedCards;
+                    usedCards=new ArrayList<>();
+                }
+                int cardHeight,cardWidth;
+                cardHeight=(int)(dm.heightPixels*0.4);
+                cardWidth=(int)(dm.widthPixels*0.4);
+                d = BitmapFactory.decodeStream(manager.open("cards/"+s));
+                d=Bitmap.createScaledBitmap(d,cardWidth,cardHeight,false);
+                String[] spl=s.split("/");
+                Card card=new Card(d,spl[spl.length-1].split("\\.")[0],null);
+                return card;
+            }catch(Exception e){}
+
+
+
 
         }
         return null;
